aa4a31d7f8b22416608c520272df21b8a6a2fdb2
AndersonQueirooz <andersonqueiroz537@gmail.com>
2025-12-06 21:32:52 -0300

    Ajustando a classe ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,24 @@
 public class ContaTerminal {
-    public static void main(String[] args) {
-        
-    }   
-    
+
+    int numeroConta;
+    String agencia;
+    String nomeCliente;
+    double saldo;
+
+    public void numeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public void agencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public void nomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public void saldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
 }
