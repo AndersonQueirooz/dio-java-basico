4449007622bfa16160cdecaa95845a3d536d6c4d
AndersonQueirooz <andersonqueiroz537@gmail.com>
2025-12-17 14:01:20 -0300

    Imprimindo-Lista-De-Candidatos-Selecionados

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -3,25 +3,35 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
 
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados() { // Método que imprime os candidatos selecionados
+        String[] candidatos = { "Ana", "Bia", "Carlos", "Daniel", "Rafaela" }; // Array com os nomes dos candidatos
+        System.out.println("Imprimindo a lista de candidatos selecionados para o processo seletivo");
+        for (int i = 0; i < candidatos.length; i++) {
+            System.out.println("O candidato selecionado foi: " + candidatos[i]);
+        }
     }
 
     static void selecaoCandidatos() { // Método que seleciona candidatos até preencher 5 vagas de acordo com o salário pretendido
         String[] candidatos = { "Ana", "Bia", "Carlos", "Daniel", "Rafaela", "Gui", "Rebeca", "Arthur", "Paula",
-                "João" };
+                "João" }; // Array com os nomes dos candidatos
         int candidatosSelecionados = 0;
         int candidatoAtual = 0;
         double salarioBase = 2000.0;
-        while (candidatosSelecionados < 5) {
-            String candidato = candidatos[candidatoAtual];
-            double valorPretendido = valorPretendido();
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) { // Enquanto não preencher 5 vagas e houver candidatos
+            String candidato = candidatos[candidatoAtual]; // Pega o nome do candidato atual
+            double salarioPretendido = valorPretendido(); // Chama a função que retorna o valor pretendido
 
-            System.out.println("O candidato " + candidato + " solicitou este valor de salário: " + valorPretendido);
-            if (salarioBase >= valorPretendido) {
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário: " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
                 System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
                 candidatosSelecionados++;
             }
             candidatoAtual++;
         }
+
     }
     
     static double valorPretendido() {
