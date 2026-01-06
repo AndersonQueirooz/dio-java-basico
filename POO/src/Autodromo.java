b61a8e9d6519da37ade345966708bc3fc38982c9
AndersonQueirooz <andersonqueiroz537@gmail.com>
2026-01-06 14:36:08 -0300

    Conhecendo-a-POO

---
+++ POO/src/Autodromo.java
@@ -0,0 +1,10 @@
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.setChassi("123456");
+        jeep.ligar();
+
+        Moto harley = new Moto();
+        harley.setChassi("654321");
+    }
+}
