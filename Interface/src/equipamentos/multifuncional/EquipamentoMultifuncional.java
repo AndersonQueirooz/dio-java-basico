27ff477ee31b49c38c16b2e543d1c7a23468c31e
AndersonQueirooz <andersonqueiroz537@gmail.com>
2026-01-09 16:17:57 -0300

    Finalizando

--- Interface/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
+++ Interface/src/equipamentos/multifuncional/EquipamentoMultifuncional.java
@@ -1,4 +1,24 @@
 package equipamentos.multifuncional;
-public class EquipamentoMultifuncional {
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Impressora;
+
+public class EquipamentoMultifuncional implements Impressora, Copiadora, Digitalizadora {
+
+    @Override
+    public void imprimir() {
+        System.out.println("Imprimindo documento via multifuncional...");
+    }
+
+    @Override
+    public void copiar() {
+        System.out.println("Copiando documento via multifuncional...");
+    }
+
+    @Override
+    public void digitalizar() {
+        System.out.println("Digitalizando documento via multifuncional...");
+    }
     
 }
--- Interface/src/estabelecimento/Fabrica.java
+++ Interface/src/estabelecimento/Fabrica.java
@@ -1,6 +1,13 @@
 package estabelecimento;
+
+import equipamentos.impressora.DeskJet;
+import equipamentos.impressora.Impressora;
+import equipamentos.multifuncional.EquipamentoMultifuncional;
+
 public class Fabrica {
     public static void main(String[] args) {
-        
+        Impressora impressora = new EquipamentoMultifuncional();
+
+        impressora.imprimir();
     }
 }
